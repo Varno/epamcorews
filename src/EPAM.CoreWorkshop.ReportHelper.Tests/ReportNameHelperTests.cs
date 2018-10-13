dee72d75503da0b8fe131f8d7547b3b47b6758c6
Окишев Алексей Александрович <Alexey_Okishev@epam.com>
2018-10-13 14:40:45 +0400

    HOL2. Add runtime-specific code

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
@@ -8,9 +9,7 @@ namespace EPAM.Core.ReportHelper.Tests
     public class ReportNameHelperTests
     {
         [TestMethod]
-        //[DataRow("Hello: my world", '_', "Hello_ my world")]
-        [DataRow("Hello my %world", '_', "Hello my %world")]
-        [DataRow("Hello my /world", '_', "Hello my _world")]
+        [DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]
         public void NormalizeFileNameTest(string name, char repl, string expected)
         {
             var result = ReportNameHelper.NormalizeFileName(name, repl);
@@ -18,6 +17,27 @@ namespace EPAM.Core.ReportHelper.Tests
             result.Should().BeEquivalentTo(expected);
         }
 
+
+        static object[][] NormalizeFileNameTestData
+        {
+            get
+            {
+                object[][] data = new object[][]
+                {
+                    new object[]{"Hello: my world", '_', "Hello_ my world"},
+                    new object[]{"Hello my %world", '_', "Hello my %world"},
+                    new object[]{"Hello my /world", '_', "Hello my _world"}
+                };
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    data[0][2] = "Hello: my world";
+                    data[1][2] = "Hello my %world";
+                    data[2][2] = "Hello my _world";
+                }
+                return data;
+            }
+        }
 
         [AssemblyInitialize]
         public static void Init(TestContext context)
